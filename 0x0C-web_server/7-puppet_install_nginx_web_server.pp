247d35540bd0a4630369f11a191cf56d0c632e9b
Diana <dimhenaopa@gmail.com>
2022-01-07 19:00:21 +0000

    :hammer: Add manifest file 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Configure an Nginx server
-exec { 'nginxinstall':
+exec { 'install':
   command  => 'sudo apt-get update; sudo apt-get -y install nginx; service nginx start; echo "Hello World" | sudo tee /var/www/html/index.html; redirect_to="server_name \/redirect_me;\n\treturn 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4; sed -i "s/server_name _;/$redirect_to/" /etc/nginx/sites-available/default; service nginx restart',
   provider => 'shell',
 }
