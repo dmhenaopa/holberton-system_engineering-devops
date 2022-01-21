23269ff682df2949fb1c946e4eef6619dd58e050
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 15:48:33 +0000

    :hammer: Update manifest file 2-puppet_custom_http_response_header.pp - remove file statement

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,11 +8,6 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-file { 'index.nginx-debian.html':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World',
-}
-
 exec { 'config':
   command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
   provider => 'shell',
