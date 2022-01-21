fc511da17a39e6ba08c0b9ea02e5e2cbd728a411
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 01:24:41 +0000

    :hammer: Upload manifest file puppet-lint 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,12 +9,7 @@ file { 'index.nginx-debian.html':
 }
 
 exec { 'config':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
-exec { 'response':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By "\$HOSTNAME" always;/" /etc/nginx/sites-available/default',
+  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\n\n\tadd_header X-Served-By \$HOSTNAME always;/" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 
