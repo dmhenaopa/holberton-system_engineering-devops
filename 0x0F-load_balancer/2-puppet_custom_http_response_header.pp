ae00f488d00dc4a3336541af4029da0bf03f385f
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 19:02:55 +0000

    :hammer: Update file - last chance - adding sudo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,20 @@
 # Create a custom HTTP header response
 exec { 'update':
-  command  => 'apt-get update',
+  command  => 'sudo apt-get update',
   provider => shell,
 }
 
 exec { 'nginx':
-  command  => 'apt-get -y install nginx',
+  command  => 'sudo apt-get -y install nginx',
   provider => shell,
 }
 
 exec { 'header':
-  command  => 'sed -i "/server_name _;/server_name _;\n\tadd_header X-Served-By "\$HOSTNAME";/" /etc/nginx/sites-available/default',
+  command  => 'sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$HOSTNAME;/" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
 exec { 'start':
-  command  => 'service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
