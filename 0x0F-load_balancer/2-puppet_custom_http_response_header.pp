b8850feadc2ac7ccd655d8991bd0ada730fd0f59
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 02:05:28 +0000

    :bug: Fix a bug - add a double quote

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,7 +9,7 @@ file { 'index.nginx-debian.html':
 }
 
 exec { 'config':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/ /etc/nginx/sites-available/default',
+  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 
