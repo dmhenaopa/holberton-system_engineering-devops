155ffd3ad493399ba3711507b8904f46a8bcedd0
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 01:19:45 +0000

    :hammer: Upload manifest file 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,8 +3,18 @@ package { 'nginx':
   ensure => 'installed',
 }
 
+file { 'index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World',
+}
+
+exec { 'config':
+  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
 exec { 'response':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By "$HOSTNAME" always;/" /etc/nginx/sites-available/default',
+  command  => 'sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By "\$HOSTNAME" always;/" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 
