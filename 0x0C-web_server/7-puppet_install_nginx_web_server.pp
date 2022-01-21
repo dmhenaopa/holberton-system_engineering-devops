fe19624fa4220935a9457c4e6f4f8b014199e003
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 00:34:29 +0000

    :hammer: Add manifest file 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,6 +3,11 @@ package { 'nginx':
   ensure => 'installed',
 }
 
+file { 'index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World',
+}
+
 exec { 'config':
   command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
   provider => 'shell',
