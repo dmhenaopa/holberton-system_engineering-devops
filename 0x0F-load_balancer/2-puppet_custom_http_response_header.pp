c44b2ea756471224a262bc1590eea2a4c4526dd0
Diana Henao <dimhenaopa@gmail.com>
2022-01-21 16:04:13 +0000

    :hammer: last chance

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,20 @@
 # Create a custom HTTP header response
 exec { 'update':
   command  => 'sudo apt-get update',
-  provider => 'shell',
+  provider => shell,
 }
 
 exec { 'nginx':
   command  => 'sudo apt-get -y install nginx',
-  provider => 'shell',
-}
-
-exec { 'config':
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
-  provider => 'shell',
+  provider => shell,
 }
 
 exec { 'header':
-  command  => 'sudo sed -i "22i add_header X-Served-By "\$HOSTNAME" always;" /etc/nginx/nginx.conf',
-  provider => 'shell',
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By "\$HOSTNAME" always;/" /etc/nginx/sites-available/default',
+  provider => shell,
 }
 
 exec { 'start':
   command  => 'sudo service nginx restart',
-  provider => 'shell',
+  provider => shell,
 }
