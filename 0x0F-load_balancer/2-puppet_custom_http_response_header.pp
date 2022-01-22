5709fe311a1dcda3449b5c8700bfe79d18b57b6a
Diana Henao <dimhenaopa@gmail.com>
2022-01-22 15:39:50 -0800

    :hammer: Update manifest file - condense all execs into one

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 # Create a custom HTTP header response
 exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
-}
-
-exec { 'nginx':
-  command  => 'sudo apt-get -y install nginx',
-  provider => shell,
-}
-
-exec { 'header':
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$HOSTNAME;/" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-
-exec { 'start':
-  command  => 'sudo service nginx restart',
+  command  => 'sudo apt-get update; 
+               sudo apt-get -y install nginx;
+	       sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$HOSTNAME;/" /etc/nginx/sites-available/default;
+	       sudo service nginx restart',
   provider => shell,
 }
