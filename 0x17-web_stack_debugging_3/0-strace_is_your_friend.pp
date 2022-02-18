06b145f4555db487e386fe6ecbbaebfd34319e87
Diana Maria Henao Parra <dimhenaopa@gmail.com>
2022-02-17 17:14:01 -0800

    :hammer: Add manifesto 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# A manifest that fix php configuration typo
+exec { 'php_configuration':
+  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  provider => 'shell',
+}
